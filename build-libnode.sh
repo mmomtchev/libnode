743771f2f356d1658211703045a345d0ef3d8956
Momtchil Momtchev <momtchil@momtchev.com>
2022-09-01 20:30:07 +0000

    add publishing

--- build-libnode.sh
+++ build-libnode.sh
@@ -3,7 +3,15 @@
 set -ex
 mkdir -p artifacts
 
-PACKAGE_VERSION=`head -1 node-16.x/ubuntu/debian/changelog | cut -f 2 -d "(" | cut -f 1 -d "~"`
+if [ -z "${BRANCH}" ]; then
+	BRANCH=16
+fi
+if [ "${BRANCH}" == "16" ]; then
+    PPA="ppa:mmomtchev/libnode"
+else
+    PPA="ppa:mmomtchev/libnode-${BRANCH}.x"
+fi
+PACKAGE_VERSION=`head -1 node-${BRANCH}.x/ubuntu/debian/changelog | cut -f 2 -d "(" | cut -f 1 -d "~"`
 NODE_VERSION=`echo ${PACKAGE_VERSION} | cut -f 1 -d "-"`
 
 if [ -z "${RELEASES}" ]; then
@@ -12,7 +20,7 @@ fi
 for RELEASE in ${RELEASES}; do
     docker build --build-arg RELEASE=${RELEASE} \
         --build-arg NODE_VERSION=${NODE_VERSION} \
-        -t mmomtchev/libnode-ubuntu-${RELEASE}:latest node-16.x
+        -t mmomtchev/libnode-ubuntu-${RELEASE}:latest node-${BRANCH}.x
 
     docker run --network none \
         --env SRC_ONLY=${SRC_ONLY} \
@@ -24,6 +32,6 @@ for RELEASE in ${RELEASES}; do
 
     (
         cd artifacts/source
-        dput --force ppa:mmomtchev/libnode node_${PACKAGE_VERSION}~${RELEASE}_source.changes
+        dput --force ${PPA} node_${PACKAGE_VERSION}~${RELEASE}_source.changes
     )
 done
