1143afeb89a03975d10f22c95f08cf3ebac7b13e
Momtchil Momtchev <momtchil@momtchev.com>
2022-09-02 09:53:01 +0000

    fix ccache

--- build-libnode.sh
+++ build-libnode.sh
@@ -14,17 +14,21 @@ fi
 PACKAGE_VERSION=`head -1 node-${BRANCH}.x/ubuntu/debian/changelog | cut -f 2 -d "(" | cut -f 1 -d "~"`
 NODE_VERSION=`echo ${PACKAGE_VERSION} | cut -f 1 -d "-"`
 
+if [ -n "${FAST}" ]; then
+    ARG_FAST="--build-arg FAST=1"
+fi
+
 if [ -z "${RELEASES}" ]; then
     RELEASES="bionic focal jammy"
 fi
 for RELEASE in ${RELEASES}; do
     docker build --build-arg RELEASE=${RELEASE} \
         --build-arg NODE_VERSION=${NODE_VERSION} \
+        ${ARG_FAST} \
         -t mmomtchev/libnode-ubuntu-${RELEASE}:latest node-${BRANCH}.x
 
     docker run --network none \
         --env SRC_ONLY=${SRC_ONLY} \
-        ${ARG_FAST} \
         -v `ccache --get-config=cache_dir`:/ccache --env CCACHE_DIR=/ccache \
         -v `pwd`/artifacts:/out \
         -v ${HOME}/.gnupg:/root/.gnupg \
